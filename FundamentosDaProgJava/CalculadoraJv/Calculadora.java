5ccd463f625f69ead706380fa97a7e017bc4c5d3
ucas tenorio <jamersontenorio88@gmail.com>
2025-06-09 21:27:35 -0300

    concertar os erros no codigo teste

--- FundamentosDaProgJava/CalculadoraJv/Calculadora.java
+++ FundamentosDaProgJava/CalculadoraJv/Calculadora.java
@@ -1,5 +1,33 @@
 package FundamentosDaProgJava.CalculadoraJv;
 
 public class Calculadora {
+    int a;
+    int b;
+    int resultado;
+    void soma() {
+        resultado = a + b;
+
+    }
+
+    void subtracao(){
+        resultado = a - b;
+        System.out.println("o valor obtido foi: " + resultado);
+    }
+
+    void divisao(){
+        if (b != 0) {
+            resultado = a / b;
+        }
+        else{
+            System.out.println("não é possivel dividir por zero");
+        }
+
+    }
+
+    void multiplicacao(){
+        resultado = a * b;
+
+    }
+
     
 }
---
+++ FundamentosDaProgJava/CalculadoraJv/teste.java
@@ -0,0 +1,17 @@
+package FundamentosDaProgJava.CalculadoraJv;
+import java.util.Scanner;
+
+public class teste {
+    public static void main(String[] args) {
+        System.out.println("Voce tem acesso as seguintes operações: Soma(+), Subtração(-), Divisão(/), Multiplicação(*)");
+
+        void escolha() {
+            Scanner sc = new Scanner(System.in);
+            System.in.println("Escolha uma operação: ");
+        } 
+        
+
+    }    
+
+    
+}
